d664474df83fc28dcbe11ac5dbfc6d9f4393f00c
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-08-14 13:34:34 +0300

    Reproduce a duplication bug

--- test/Main.hs
+++ test/Main.hs
@@ -42,7 +42,7 @@ main =
         atomically $
         UnfoldM.foldlM' (\ state element -> return (element : state)) [] (Hamt.unfoldM hamt)
 
-      listToListThruHamtInIo :: [(Char, Int)] -> IO [(Char, Int)]
+      listToListThruHamtInIo :: (Eq key, Hashable key, Eq value, Show (key, value)) => [(key, value)] -> IO [(key, value)]
       listToListThruHamtInIo = hamtFromListUsingInsertInIo >=> hamtToListInIo
 
       testTransactionProperty :: String -> (Text -> Int) -> Gen Transaction -> TestTree
@@ -89,6 +89,16 @@ main =
             in do
               hamtList <- listToListThruHamtInIo list
               assertEqual (show hamtList) (delete ('b', 1) list) hamtList
+          ,
+          testCase "insert text with dups" $ let
+            list :: [(Text, Int)]
+            list =
+              [("\44825\v8<\178sV\37709\59477\EM\n\SYN\34862\45730\57533\1643\1958i8\65022F]B\54233\9429A\RS\1797\54979\580@\2006\3400\ETX\ACK\63557\50825C\3741 \238\54817P\3258\54517\1081F5\16070\NAKR\1539\1193S\33229\1726&\778\20733\250\857\712C)\SYN1\17881\DC2\702\1446\61050S\5170s\ENQ\826\1379[7\53746\46137\55010\&0\1518:\2827R\54715\n\DEL\33260\1966\664\58929\64301\839\2980",0),("",0),("",0)]
+            expectedList =
+              sort (nubBy (\ a b -> fst a == fst b) list)
+            in do
+              hamtList <- listToListThruHamtInIo list
+              assertEqual (show hamtList) expectedList (sort hamtList)
           ,
           testTransactionProperty "insert" hash Gens.insertTransaction
           ,
